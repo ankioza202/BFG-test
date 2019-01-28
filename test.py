2983fa02a3170bb357104f1d5495dce05b363bcd
Ankita <ankita_oza@comcast.com>
2019-01-28 08:24:24 -0500

    Adding credentials to script

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
+from datetime import datetime
 
+import cx_Oracle
+import sys
+import os
+
+
+
+DEMO_USERNAME = "ankioza"
+DEMO_PASSWORD = "TEST "
+DEMO_PORT = "1521"
+DEMO_HOST = "localhost"
+DEMO_SID = "test"
